10d6addd498d5162bd611ee58dd2d863e81fdfb8
Alden0012 <ajs19@ic.ac.uk>
2021-03-04 17:22:26 +0400

    Made basic server and multi-client file, client sends text to server which prints it out

---
+++ Client.py
@@ -0,0 +1,13 @@
+import socket
+class Client:
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	def __init__(self, address):
+		self.sock.connect((address,10000))
+		while True:
+			self.sendMsg()
+			#print("Sending")
+
+	def sendMsg(self):
+		self.sock.send(bytes(input(""), 'utf-8'))
+
+client_inst = Client('127.0.0.1')
---
+++ Server.py
@@ -0,0 +1,29 @@
+import socket
+import threading
+class Server:
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	connections = []
+	def __init__(self):
+		self.sock.bind(('0.0.0.0', 10000))
+		self.sock.listen(1)
+	def handler(self,c,a):
+		while True:
+			data = c.recv(1024)
+			alias = str([a[0]]) + ":" + str(a[1])
+			if not data:
+				print(alias, "disconnected" )
+				self.connections.remove(c)
+				c.close()
+				break;
+			print(alias + ' says: ' + str(data,'utf-8'))
+	def run(self):
+		while True:
+				c, a = self.sock.accept()
+				cThread = threading.Thread(target = self.handler, args = (c,a))
+				cThread.daemon = True
+				cThread.start()
+				self.connections.append(c)
+				print(str([a[0]]) + ":" + str(a[1]), "connected" )
+
+server_inst = Server()
+server_inst.run()
