e6a82c9e8eb47a49aacc47eb133c93017361873f
root <root@DESKTOP-V3V34VD.localdomain>
2021-03-17 13:33:17 +0400

    Added game functionality

--- Client.py
+++ Client.py
@@ -2,34 +2,62 @@
 import threading
 import socket
 import subprocess
+import time
+import os
 class Client:
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	started = False
 	def __init__(self, address):
 		self.sock.connect((address,10000))
-		self.run()
-	def run(self):
-		aThread = threading.Thread(target = self.recieveMsg)
+		self.runS1()
+	def runS1(self): #connecting to server state
+		data = self.sock.recv(1024)
+		print(str(data,'utf-8'))
+		#while !(str(data,'utf-8')):
+		#	data = self.sock.recv(1024)
+	    #	print(str(data,'utf-8'))
+		self.runS2()
+	def runS2(self): #game state
+		print("Game Started!")
+		started = True
+		aThread = threading.Thread(target = self.recieveResponse)
 		aThread.daemon = True
-		aThread.start()	
-		while True:
+		aThread.start()
+		while started:
+			time.sleep(1.3)
 			self.sendMsg()
-			#print("Sending")
-	def recieveMsg(self):
+		aThread.stop()
+		print("Thanks for playing!")
+		data = self.sock.recv(1024)
+		print(str(data,'utf-8'))
+	def wait(self):
+		time.sleep(8)
+		started = False
+	def recieveResponse(self):
 		while True:
 			data = self.sock.recv(1024)
-			if not data:
+			print(str(data,'utf-8'))
+			if(str(data,'utf-8') == "e" or str(data,'utf-8') == ""):
+				started = False
 				break
-			print('Server: ' + str(data,'utf-8'))	
+		print("Thanks for playing!")
+		os._exit(0)
+	def recieveMsg(self):
+		while True:
+
+			data = self.sock.recv(1024)
+			self.runS2()				
+			
 	def sendMsg(self):
-		self.sock.send(bytes(input(""), 'utf-8'))
-		self.run()
+		self.sock.send(bytes("A", 'utf-8'))
+
 	def ping_jtag(self):
 		#pings the jtag with the letter t, when the fgpa recieves this through the jtag
 		#it will respond with current accelerometer 
 		inputCmd = "nios2-terminal <<< t"
 		output = subprocess.run(inputCmd, shell=True, executable='/bin/bash', stdout=subprocess.PIPE)
 		vals = output.stdout
-		return vals;
+		return vals
     	
 
-client_inst = Client('104.45.152.207')
+client_inst = Client('127.0.0.1')
--- Server.py
+++ Server.py
@@ -1,9 +1,15 @@
 import socket
 import threading
-
+import time
+import random
 class Server:
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	connections = []
+	QuestionBank = {"Choose A": "A", "Choose B": "B" ,"Choose C": "C" ,"Choose D": "D"}
+	Players = {}
+	Recieved = {}
+	AliasToC = {}
+	maxPlayers = 2
 	def __init__(self):
 		self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		self.sock.bind(('0.0.0.0', 10000))
@@ -11,25 +17,75 @@ class Server:
 	def handler(self,c,a):
 		while True:
 			data = c.recv(1024)
-			alias = str([a[0]]) + ":" + str(a[1])
+			alias = a
 			if not data:
 				print(alias, "disconnected" )
 				self.connections.remove(c)
 				c.close()
-				break;
-			print(alias + ' says: ' + str(data,'utf-8'))
-			self.broadcast_single(data);
-	def run(self):
+				break
+			self.Recieved[alias] = str(data,'utf-8')
+			#print(alias + ' says: ' + str(data,'utf-8'))
+			print(self.Recieved)
+			#self.broadcast_single(data)
+	def runS1(self):
+		#State 1: Connecting to other devices
 		while True:
 				c, a = self.sock.accept()
-				cThread = threading.Thread(target = self.handler, args = (c,a))
+				#cThread = threading.Thread(target = self.handler, args = (c,a))
+				#cThread.daemon = True
+				#cThread.start()
+				#aThread = threading.Thread(target = self.broadcast)
+				#aThread.daemon = True
+				#aThread.start()
+				self.connections.append(c)
+				alias = str([a[0]]) + ":" + str(a[1])
+				self.Players[alias] = 0
+				self.Recieved[alias] = 0
+				self.AliasToC[alias] = c
+				print(str([a[0]]) + ":" + str(a[1]), "connected" )
+				
+				if len(self.connections) > self.maxPlayers-1:
+					break
+		print("All players connected! Beginning Game")
+		time.sleep(3)
+		for connection in self.connections:
+					connection.send(bytes('c', 'utf-8'))
+		self.runS2();				
+	def runS2(self):
+		for connection in self.connections:
+			connection.send(bytes("Get Ready!" + "\n", 'utf-8'))
+		i = 1
+		while i < 3: 
+			#for connection in self.connections:
+			#	connection.send(bytes("Round " + str(i), 'utf-8'))
+			Question, answer = random.choice(list(self.QuestionBank.items())) 
+			for connection in self.connections:
+				connection.send(bytes(Question, 'utf-8'))
+			for key in self.AliasToC.keys():
+				cThread = threading.Thread(target = self.handler, args = (self.AliasToC[key],key))
 				cThread.daemon = True
 				cThread.start()
-				aThread = threading.Thread(target = self.broadcast)
-				aThread.daemon = True
-				aThread.start()
-				self.connections.append(c)
-				print(str([a[0]]) + ":" + str(a[1]), "connected" )
+			time.sleep(10)
+			print("Round Over!")
+			print(self.Recieved)
+			for connection in self.connections:
+				connection.send(bytes("Round " + str(i) + " over!"+ "\n", 'utf-8'))
+			for key in self.Recieved.keys():
+				if answer == self.Recieved[key]:
+					self.Players[key] += 1
+					self.AliasToC[key].send(bytes("Correct!\n" + "\n", 'utf-8'))
+					self.AliasToC[key].send(bytes("Points: " + str(self.Players[key]) + "\n", 'utf-8'))
+				else:
+					self.AliasToC[key].send(bytes("Incorrect!\n", 'utf-8'))
+					self.AliasToC[key].send(bytes("Points: " + str(self.Players[key]) + "\n", 'utf-8'))
+			print(self.Players)
+			i = i + 1
+		for connection in self.connections:
+				connection.send(bytes("Game over!"+ "\n", 'utf-8'))
+				connection.send(bytes(str(self.Players)+ "\n", 'utf-8'))
+				connection.send(bytes(str("e")+ "\n", 'utf-8'))
+
+
 	def broadcast_single(self,data):
 		for connection in self.connections:
 				connection.send(data)
@@ -44,4 +100,4 @@ class Server:
 				exit()
 
 server_inst = Server()
-server_inst.run()
+server_inst.runS1()
